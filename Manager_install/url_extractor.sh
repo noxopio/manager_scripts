cef09b54cd4ea38a9c9ae637c2bb048da2a83d06
nox <dark.tempus@gmail.com>
2025-03-28 10:30:35 -0500

    feat: Url refactor

--- Manager_install/url_extractor.sh
+++ Manager_install/url_extractor.sh
@@ -145,21 +145,66 @@ extract_urls() {
     echo "$1" | grep -o '"html_url": "[^"]*' | sed 's/"html_url": "//' | sort | uniq
 }
 fetch_repositories() {
-    local headers=()
-    [ -n "$TOKEN" ] && headers=(-H "Authorization: token $TOKEN")
-    while true; do
-        response=$(curl "${headers[@]}" -s -I "$URL")
-        body=$(curl "${headers[@]}" -s "$URL")
+if [ -n "$TOKEN" ]; then
+  # Realizar la solicitud GET a la API con token
+log_info "Accediendo a repositorios privados con token..."
+  while true; do
+    response=$(curl -H "Authorization: token $TOKEN" -s -I "$URL")
+    body=$(curl -H "Authorization: token $TOKEN" -s "$URL")
 
-        extract_urls "$body" >> listRep.txt
+    # Extraer las URLs de los repositorios y añadirlas al archivo
+    extract_urls "$body" >> listRep.txt
 
-        next_page=$(echo "$response" | grep -i "Link: " | grep -o '<[^>]*>; rel="next"' | sed -e 's/<\(.*\)>; rel="next"/\1/')
-        if [ -z "$next_page" ]; then
-            break
-        fi
-        URL="$next_page"
-    done
+    # Verificar si hay más páginas (esto lo indica el encabezado "Link")
+    next_page=$(echo "$response" | grep -i "Link: " | grep -o '<[^>]*>; rel="next"' | sed -e 's/<\(.*\)>; rel="next"/\1/')
+
+    if [ -z "$next_page" ]; then
+      break
+    fi
+
+    # Actualizar la URL con el enlace de la siguiente página
+    URL="$next_page"
+  done
+
+else
+  # Acceso a repositorios públicos
+ log_info "Accediendo a repositorios públicos..."
+  while true; do
+    response=$(curl -s -I "$URL")
+    body=$(curl -s "$URL")
+
+    # Extraer las URLs de los repositorios y añadirlas al archivo
+    extract_urls "$body" >> listRep.txt
+
+    # Verificar si hay más páginas (esto lo indica el encabezado "Link")
+    next_page=$(echo "$response" | grep -i "Link: " | grep -o '<[^>]*>; rel="next"' | sed -e 's/<\(.*\)>; rel="next"/\1/')
+
+    if [ -z "$next_page" ]; then
+      break
+    fi
+
+    # Actualizar la URL con el enlace de la siguiente página
+    URL="$next_page"
+  done
+fi
 }
+
+# fetch_repositories() {
+#     local headers=()
+#     [ -n "$TOKEN" ] && headers=(-H "Authorization: token $TOKEN")
+#     while true; do
+#         response=$(curl "${headers[@]}" -s -I "$URL")
+#         body=$(curl "${headers[@]}" -s "$URL")
+
+#         extract_urls "$body" >> listRep.txt
+
+#         next_page=$(echo "$response" | grep -i "Link: " | grep -o '<[^>]*>; rel="next"' | sed -e 's/<\(.*\)>; rel="next"/\1/')
+#         if [ -z "$next_page" ]; then
+#             break
+#         fi
+#         URL="$next_page"
+#     done
+# }
 
 
 end_process() {
