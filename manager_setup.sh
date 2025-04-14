5d3a08e1d1c6871f151def4fdcd36210ea39bf40
nox <dark.tempus@gmail.com>
2025-04-14 17:49:00 -0500

    feat: Actualizar manager_setup

--- manager_setup.sh
+++ manager_setup.sh
@@ -1,43 +1,27 @@
 #!/bin/bash
 # manager_setup.sh
-# Script para clonar, descomprimir e instalar Manager.
-# Este script está diseñado para descargar la última versión del repositorio Manager
-# desde la rama principal de GitHub, preparar los archivos necesarios y proceder
-# con la instalación automatizada del sistema Manager.
-#
-# Funciones incluidas:
-# 1. clone_repo: Clona la última versión del repositorio desde la rama principal.
-# 2. decompress_repo: Descomprime los archivos necesarios para la instalación.
-# 3. install: Realiza la instalación del sistema Manager según las opciones del usuario.
-#
-# Uso:
-# Ejecuta este script en tu terminal y sigue las instrucciones.
+# Script para clonar, preparar e instalar Manager.
 
 set -e
 
 # Función para clonar el repositorio
 clone_repo() {
     echo "Clonando el repositorio..."
-    curl -L -o repo.zip https://github.com/noxopio/manager_scripts/archive/refs/heads/main.zip
-    if [ ! -f repo.zip ]; then
-        echo "Error: No se pudo descargar el repositorio. Verifica la URL."
+    if git clone --depth=1 https://github.com/noxopio/manager_scripts.git; then
+        echo "Repositorio clonado con éxito."
+    else
+        echo "Error: No se pudo clonar el repositorio. Verifique la URL o la conexión a Internet."
         exit 1
     fi
-}
 
-# Función para descomprimir los archivos
-decompress_repo() {
-    echo "Descomprimiendo los archivos..."
-    unzip -q repo.zip "manager_scripts-main/Manager_install/*" || {
-        echo "Error: Falló la descompresión del archivo."
-        exit 1
-    }
-    if [ ! -d "manager_scripts-main/Manager_install" ]; then
-        echo "Error: No se encontraron los archivos necesarios tras la descompresión."
+    # Verificar y mover el directorio necesario
+    if [ -d "manager_scripts/Manager_install" ]; then
+        mv manager_scripts/Manager_install .
+        rm -rf manager_scripts
+    else
+        echo "Error: No se encontró el directorio Manager_install en el repositorio clonado."
         exit 1
     fi
-    mv manager_scripts-main/Manager_install .
-    rm -rf manager_scripts-main repo.zip
 }
 
 # Función para instalar
@@ -63,12 +47,9 @@ install() {
     esac
 }
 
-# Función principal
 main() {
     clone_repo
-    decompress_repo
     install
 }
 
-# Llamada a la función principal
 main "$@"
